1dc6d99ed42c7470b50bce9a17d630c2c03151d6
dowon2308 <dowon2308@gmail.com>
2012-05-28 11:43:50 +0000

    branchs 추가 및 포인트 선물 모듈 커밋

---
+++ pointsend/lang/en.lang.php
@@ -0,0 +1,70 @@
+<?php
+    /**
+     * @file en.lang.php
+     * @author SMaker (dowon2308@paran.com)
+     * @brief  Default Language Pack of PointGift Module
+     **/
+    $lang->point = 'Point';
+    $lang->pointsend = $lang->pointgift = 'Point Gift';
+    $lang->fee = 'Fee';
+    $lang->fee_persent = 'Fee Persent';
+    $lang->fee_apply_point = 'Fee Apply Point';
+    $lang->fee_but_group = '수수료 제외 그룹';
+
+    $lang->send_point = '보낼 포인트';
+    $lang->current_point = 'Current Point';
+    $lang->after_point = 'After Point';
+
+    $lang->cmd_pointsend = 'Point Gift';
+    $lang->cmd_find_member = 'Find Member';
+    $lang->cmd_pointsend_log_list = 'Point Gift Log';
+    $lang->cmd_group_pointsend = '그룹별 포인트 선물';
+    $lang->cmd_member_pointsend = '회원별 포인트 선물';
+    $lang->cmd_all_pointsend = '전체 회원 포인트 선물';
+
+    $lang->success_pointsend = 'Point Gift Successful';
+
+    $lang->pointsend_title = '[Notice] You received a Point Gift from %s.';
+    $lang->pointsend_content = 'You received a Point Gift from %s (%s).<br /><br /><strong>※ This message was sent automatically.</strong><br /><br />----------------------------<br />Message : <br />%s';
+
+    $lang->about_send_message = 'Send Message';
+    $lang->about_send_group = '<h4 class="xeAdmin">Replacement</h4>
+    <div class="infoText">치환자를 사용하여 다수의 회원에게 다른 내용의 쪽지를 보내실 수 있습니다.</div>
+    <ul>
+        <li><strong>%_SENDER_%</strong> : Sender (Use Member Extra Info)</li>
+        <li><strong>%_RECEIVER_%</strong> : Receiver (Use Member Extra Info)</li>
+        <li><strong>%SENDER%</strong> : Sender Nick Name</li>
+        <li><strong>%SENDER_ID%</strong> : Sender ID</li>
+        <li><strong>%SENDER_SRL%</strong> : Sender Member No.</li>
+        <li><strong>%RECEIVER%</strong> : Receiver Nick Name</li>
+        <li><strong>%RECEIVER_ID%</strong> : Receiver ID</li>
+        <li><strong>%RECEIVER_SRL%</strong> : Receiver Member No.</li>
+        <li><strong>%POINT%</strong> : Point</li>';
+    $lang->about_deny_group = '특정 그룹이 포인트 선물을 받지 못하게 합니다.';
+
+    $lang->confirm_pointsend = 'Are you sure to Point Gift?';
+
+    $lang->cmd_view_log = array(
+        'R' => 'Received Point',
+        'S' => 'Sent Point'
+    );
+
+    $lang->pointgift_limit = 'Point Gift Limit';
+    $lang->pointgift_denied_group = 'Point Gift Denied Group';
+    $lang->pointgift_daily_limit = 'Daily Point Gift Limit';
+
+    $lang->msg_need_login = 'You need login.';
+    $lang->msg_not_exists_member = 'Member does not exist';
+    $lang->msg_not_exists_sender = 'Sender does not exist';
+    $lang->msg_not_exists_receiver = 'Receiver does not exist';
+    $lang->msg_not_enough_send_point = '보낼 포인트가 현재 보유하고 있는 포인트 보다 많습니다.';
+    $lang->msg_invalid_send_point = '보낼 포인트를 올바르게 입력해 주세요.\n\n보낼 포인트는 0 보다 커야 합니다.';
+    $lang->msg_not_permitted_pointsend = 'You do not have permission to point gift';
+    $lang->msg_pointgift_daily_limit_over = 'You exceed the Daily Point Gift Limit (%s Point).';
+    $lang->msg_pointgift_denied_group = 'You can\'t point gift to Denied Group (%s).';
+    $lang->msg_not_exists_pointgift_log = 'Point Gift log isn\'t exists';
+
+    $lang->cmd_pointsend_cancle = 'Point Gift Cancel';
+    $lang->about_pointsend_cancle = 'Are you sure to cancel the point gift?<br />
+    If you cancel the gift, send the message to sender and receiver.<br /><br />';
+?>
---
+++ pointsend/lang/jp.lang.php
@@ -0,0 +1,42 @@
+<?php
+    /**
+     * @file jp.lang.php
+     * @author SMaker (dowon2308@paran.com) 翻訳：HIRO
+     * @brief  pointsend 日本語パッケージ
+     **/
+    $lang->point = 'ポイント';
+    $lang->pointsend = 'ポイント贈り物';
+    $lang->fee = '手数料';
+    $lang->fee_persent = '手数料の割合';
+    $lang->fee_apply_point = '手数料適用ポイント';
+    $lang->fee_but_group = '手数料除外グループ';
+
+    $lang->send_point = '送るポイント';
+    $lang->current_point = '現在ポイント';
+
+    $lang->cmd_find_member = '会員探し';
+    $lang->cmd_pointsend_log_list = 'ポイント贈り物内訳';
+
+    $lang->msg_need_login = 'ログインが必要です.';
+    $lang->msg_not_exists_member = '存在しない会員です.';
+    $lang->msg_not_exists_receiver = '受信者が存在しない会員です.';
+    $lang->msg_not_enough_send_point = '送るポイントが現在保有しているポイントより多いです.';
+    $lang->msg_invalid_send_point = '送るポイントを正しく入力してください.\n\n送るポイントは 0 より大きくなければなりません.';
+    $lang->msg_not_permitted_pointsend = 'ポイント贈り物権限がありません.';
+
+    $lang->success_pointsend = '正常にポイントがプレゼントされました.';
+
+    $lang->pointsend_title = '[お知らせ] %s様からポイントをプレゼントされました.';
+    $lang->pointsend_content = '%s (%s) 様から %s ポイントをプレゼントされました.<br /><br /><strong>※ このメッセージは自動に発送されたメッセージです.</strong><br /><br />----------------------------<br />メッセージ内容 : <br />%s';
+
+    $lang->about_send_message = '포인트 선물 성공 시 쪽지를 보냅니다.';
+    $lang->about_send_group = '{sender} : 보내는이 닉네임<br />
+{sender_id} : 보내는이 아이디<br />
+{receiver} : 받는이 닉네임<br />
+{receiver_id} : 받는이 아이디<br />
+{send_point} : 보낸 포인트<br />
+{received_point} : 받은 포인트
+';
+
+    $lang->confirm_pointsend = '정말 포인트를 선물하시겠습니까?';
+?>
