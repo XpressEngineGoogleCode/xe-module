72fa75e9eb2b09b019d8c076e90c874faafe623a
dowon2308@gmail.com <dowon2308@gmail.com@c953d14b-ce9f-cf57-ae3d-9ac39517c60f>
2012-08-27 11:46:54 +0000

    clean code

--- branches/pointsend/1.2.0/pointsend.admin.controller.php
+++ branches/pointsend/1.2.0/pointsend.admin.controller.php
@@ -1,132 +1,133 @@
 <?php
+/**
+ * @class  pointsendAdminController
+ * @author 퍼니엑스이 (admin@funnyxe.com)
+ * @brief  pointsend 모듈의 Admin Controller class
+ **/
+
+class pointsendAdminController extends pointsend {
 	/**
-	 * @class  pointsendAdminController
-	 * @author SMaker (dowon2308@paran.com)
-	 * @brief  pointsend 모듈의 Admin Controller class
+	 * @brief 초기화
 	 **/
-
-	class pointsendAdminController extends pointsend {
-
-		/**
-		 * @brief 초기화
-		 **/
-		function init() {
-		}
-
-		/**
-		 * @brief 설정 저장
-		 */ 
-		function procPointsendAdminInsertConfig() {
-			// 입력받은 설정 항목을 구함
-			$config = Context::getRequestVars();
-
-			// 불필요한 항목 제외
-			unset($config->module);
-			unset($config->act);
-			unset($config->body);
-			unset($config->_filter);
-
-			// 기본값 지정
-			if(!$config->skin) $config->skin = 'default';
-			if(!$config->use_fee) $config->use_fee = 'N';
-			if(!$config->sameip_deny) $config->sameip_deny = 'N';
-
-			$oModuleController = &getController('module');
-			$oModuleController->insertModuleConfig('pointsend', $config);
-
-			$this->setMessage('success_saved');
-		}
-
-		/**
-		 * @brief 포인트 선물 취소 (관리자용)
-		 */
-		function procPointsendAdminRollback() {
-			$log_srl = (int)Context::get('log_srl');
-			if(!$log_srl) return new Object(-1, 'msg_invalid_request');
-
-			$oPointsendModel = &getModel('pointsend');
-			$log_info = $oPointsendModel->getLogInfoByLogSrl($log_srl);
-
-			$sender_srl = (int)$log_info->sender_srl;
-			$receiver_srl = (int)$log_info->receiver_srl;
-
-			if(!$sender_srl || !$receiver_srl) return new Object(-1, 'msg_invalid_request');
-
-			$oMemberModel = &getModel('member');
-			$config = $oPointsendModel->getConfig();
-
-			$point = $log_info->point;
-			$sender_point = $point;
-			$receiver_point = $point;
-
-			$sender_info = $oMemberModel->getMemberInfoByMemberSrl($sender_srl);
-			$receiver_info = $oMemberModel->getMemberInfoByMemberSrl($receiver_srl);
-
-			// 수수료에 따른 포인트 계산
-			$fee_per = (int)$config->fee;
-			$fee_apply_point = (int)$config->fee_apply_point;
-			$fee = 0;
-			if($config->use_fee == 'Y' && $fee_per && $point>=$fee_apply_point) $fee = $point * ($config->fee/100);
-
-			$receiver_point -= $fee;
-
-			$oPointController = &getController('point');
-			$oPointController->setPoint($sender_srl, $sender_point, 'add');
-			$oPointController->setPoint($receiver_srl, $receiver_point, 'minus');
-
-			// 쪽지 보내기
-			$title = Context::getLang('pointsendc_title');
-			$content = sprintf(Context::getLang('pointsendc_content2'), $sender_info->nick_name, $sender_info->user_id, $receiver_point);
-			$content2 = sprintf(Context::getLang('pointsendc_content'), $receiver_info->nick_name, $receiver_info->user_id, $sender_point);
-
-			$oCommunicationController = &getController('communication');
-			$oCommunicationController->sendMessage($sender_srl, $receiver_srl, $title, $content, false);
-			$oCommunicationController->sendMessage($receiver_srl, $sender_srl, $title, $content2, false);
-
-			$args->log_srl = $log_srl;
-			executeQuery('pointsend.deletePointsendLog',$args);
-		}
-
-		/**
-		 * @brief 일괄 포인트 선물 - 그룹별
-		 */
-		function procPointsendAdminSendGroup() {
-			$cart = Context::get('cart');
-			if(!$cart) return new Object(-1, 'msg_invalid_request');
-
-			$group_srls = str_replace('|@|', ',', Context::get('cart'));
-			$send_point = Context::get('send_point');
-			$title = Context::get('title');
-			$content = Context::get('content');
-
-			$oController = &getController('pointsend');
-			$oController->pointsendToGroup($group_srls, $send_point, $title, $content);
-
-			$total = $this->get('group_count');
-			$success = $this->get('success_group');
-			$failed = $this->get('failed_group');
-			$ignore = $this->get('ignore_group');
-
-			$msg = sprintf(Context::getLang('success_group_pointgift'), $total, $success, $failed, $ignore);
-			$this->setMessage($msg);
-		}
-
-		/**
-		 * @brief 포인트 선물 내역 삭제
-		 */
-		function procPointsendAdminDeleteLog() {
-			$log_srl = Context::get('log_srl');
-			if(!$log_srl) return new Object(-1, 'msg_invalid_request');
-
-			// 삭제
-			$oController = &getController('pointsend');
-			$output = $oController->deleteLog($log_srl);
-
-			// 에러가 발생하면
-			if(!$output->toBool()) return $output;
-
-			// 메시지 지정
-			$this->setMessage('success_deleted');
-		}
+	function init() {
 	}
-?>
+
+	/**
+	 * @brief 설정 저장
+	 */ 
+	function procPointsendAdminInsertConfig() {
+		// 입력받은 설정 항목을 구함
+		$config = Context::getRequestVars();
+
+		// 불필요한 항목 제외
+		unset($config->module);
+		unset($config->act);
+		unset($config->body);
+		unset($config->_filter);
+
+		// 기본값 지정
+		if(!$config->skin) $config->skin = 'default';
+		if(!$config->use_fee) $config->use_fee = 'N';
+		if(!$config->sameip_deny) $config->sameip_deny = 'N';
+
+		$oModuleController = &getController('module');
+		$oModuleController->insertModuleConfig('pointsend', $config);
+
+		$this->setMessage('success_saved');
+	}
+
+	/**
+	 * @brief 포인트 선물 취소 (관리자용)
+	 */
+	function procPointsendAdminRollback() {
+		$log_srl = (int)Context::get('log_srl');
+		if(!$log_srl) return new Object(-1, 'msg_invalid_request');
+
+		$oPointsendModel = &getModel('pointsend');
+		$log_info = $oPointsendModel->getLogInfoByLogSrl($log_srl);
+
+		$sender_srl = (int)$log_info->sender_srl;
+		$receiver_srl = (int)$log_info->receiver_srl;
+
+		if(!$sender_srl || !$receiver_srl) return new Object(-1, 'msg_invalid_request');
+
+		$oMemberModel = &getModel('member');
+		$config = $oPointsendModel->getConfig();
+
+		$point = $log_info->point;
+		$sender_point = $point;
+		$receiver_point = $point;
+
+		$sender_info = $oMemberModel->getMemberInfoByMemberSrl($sender_srl);
+		$receiver_info = $oMemberModel->getMemberInfoByMemberSrl($receiver_srl);
+
+		// 수수료에 따른 포인트 계산
+		$fee_per = (int)$config->fee;
+		$fee_apply_point = (int)$config->fee_apply_point;
+		$fee = 0;
+		if($config->use_fee == 'Y' && $fee_per && $point>=$fee_apply_point) $fee = $point * ($config->fee/100);
+
+		$receiver_point -= $fee;
+
+		$oPointController = &getController('point');
+		$oPointController->setPoint($sender_srl, $sender_point, 'add');
+		$oPointController->setPoint($receiver_srl, $receiver_point, 'minus');
+
+		// 쪽지 보내기
+		$title = Context::getLang('pointsendc_title');
+		$content = sprintf(Context::getLang('pointsendc_content2'), $sender_info->nick_name, $sender_info->user_id, $receiver_point);
+		$content2 = sprintf(Context::getLang('pointsendc_content'), $receiver_info->nick_name, $receiver_info->user_id, $sender_point);
+
+		$oCommunicationController = &getController('communication');
+		$oCommunicationController->sendMessage($sender_srl, $receiver_srl, $title, $content, false);
+		$oCommunicationController->sendMessage($receiver_srl, $sender_srl, $title, $content2, false);
+
+		$args->log_srl = $log_srl;
+		executeQuery('pointsend.deletePointsendLog',$args);
+	}
+
+	/**
+	 * @brief 일괄 포인트 선물 - 그룹별
+	 */
+	function procPointsendAdminSendGroup() {
+		$cart = Context::get('cart');
+		if(!$cart) return new Object(-1, 'msg_invalid_request');
+
+		$group_srls = str_replace('|@|', ',', Context::get('cart'));
+		$send_point = Context::get('send_point');
+		$title = Context::get('title');
+		$content = Context::get('content');
+
+		$oController = &getController('pointsend');
+		$oController->pointsendToGroup($group_srls, $send_point, $title, $content);
+
+		$total = $this->get('group_count');
+		$success = $this->get('success_group');
+		$failed = $this->get('failed_group');
+		$ignore = $this->get('ignore_group');
+
+		$msg = sprintf(Context::getLang('success_group_pointgift'), $total, $success, $failed, $ignore);
+		$this->setMessage($msg);
+	}
+
+	/**
+	 * @brief 포인트 선물 내역 삭제
+	 */
+	function procPointsendAdminDeleteLog() {
+		$log_srl = Context::get('log_srl');
+		if(!$log_srl) return new Object(-1, 'msg_invalid_request');
+
+		// 삭제
+		$oController = &getController('pointsend');
+		$output = $oController->deleteLog($log_srl);
+
+		// 에러가 발생하면
+		if(!$output->toBool()) return $output;
+
+		// 메시지 지정
+		$this->setMessage('success_deleted');
+	}
+}
+
+/* End of file : pointsend.admin.controller.php */
+/* Location : ./modules/pointsend/pointsend.admin.controller.php */
--- branches/pointsend/1.2.0/pointsend.model.php
+++ branches/pointsend/1.2.0/pointsend.model.php
@@ -1,233 +1,256 @@
 <?php
-	/**
-	 * @class  pointsendModel
-	 * @author SMaker (dowon2308@paran.com)
-	 * @brief pointsend 모듈의 Model class
-	 **/
+/**
+ * @class  pointsendModel
+ * @author 퍼니엑스이 (admin@funnyxe.com)
+ * @brief pointsend 모듈의 Model class
+ **/
 
-	class pointsendModel extends pointsend {
+class pointsendModel extends pointsend
+{
 
-		/**
-		 * @brief 초기화
-		 **/
-		function init() {
-		}
+	/**
+	 * @brief 초기화
+	 **/
+	function init()
+	{
+	}
 
-		/**
-		 * @brief 포인트 선물 모듈의 권한 설정을 구함
-		 */
-		function getGrantInfo() {
-			static $module_config = null;
-			if(is_null($module_config)) {
-				$oModuleModel = &getModel('module');
-				$module_config = $oModuleModel->getModuleConfig('pointsend');
-				$module_config->grants = $config->grants ? explode('|@|',$config->grants) : array();
-			}
+	/**
+	 * @brief 포인트 선물 모듈의 권한 설정을 구함
+	 */
+	function getGrantInfo()
+	{
+		static $module_config = null;
+		if(is_null($module_config))
+		{
+			$oModuleModel = &getModel('module');
+			$module_config = $oModuleModel->getModuleConfig('pointsend');
+			$module_config->grants = $config->grants ? explode('|@|',$config->grants) : array();
+		}
 
-			return $module_config->grants;
-		}
+		return $module_config->grants;
+	}
 
-		/**
-		 * @brief 포인트 선물 모듈의 설정을 구함
-		 **/
-		function getConfig($is_admin = false) {
-			static $config = null;
-			if(is_null($config)) {
-				// 모듈 설정을 구함
-				$oModuleModel = &getModel('module');
-				$config = $oModuleModel->getModuleConfig('pointsend');
-				$config->skin = !$config->skin ? 'default' : $config->skin;
-				$config->use_fee = $config->use_fee == 'Y' ? 'Y' : 'N';
-				$config->fee_but_group = $config->fee_but_group ? explode('|@|',$config->fee_but_group) : array();
-				$config->deny_group = $config->deny_group ? explode('|@|',$config->deny_group) : array();
-				$config->grants = $config->grants ? explode('|@|',$config->grants) : array();
+	/**
+	 * @brief 포인트 선물 모듈의 설정을 구함
+	 **/
+	function getConfig($is_admin = false)
+	{
+		static $config = null;
+		if(is_null($config))
+		{
+			// 모듈 설정을 구함
+			$oModuleModel = &getModel('module');
+			$config = $oModuleModel->getModuleConfig('pointsend');
+			$config->skin = !$config->skin ? 'default' : $config->skin;
+			$config->use_fee = $config->use_fee == 'Y' ? 'Y' : 'N';
+			$config->fee_but_group = $config->fee_but_group ? explode('|@|',$config->fee_but_group) : array();
+			$config->deny_group = $config->deny_group ? explode('|@|',$config->deny_group) : array();
+			$config->grants = $config->grants ? explode('|@|',$config->grants) : array();
 
-				unset($config->module);
-				unset($config->act);
-				unset($config->body);
-				unset($config->_filter);
+			unset($config->module);
+			unset($config->act);
+			unset($config->body);
+			unset($config->_filter);
 
-				$logged_info = Context::get('logged_info');
-
-				$tmp_group = array();
-
-				if($logged_info) {
-					foreach($config as $key => $val) {
-						if(!preg_match('/^fee_([0-9]+)$/i', $key, $matches)) continue;
-
-						$fee = $val < 1 ? 0 : $val;
-						$fee = $val > 99 ? 99 : $val;
-						$config->fee_[$matches[1]] = $fee;
-
-						if(!$is_admin) {
-							$tmp_group[$matches[1]] = $matches[1];
-							$tmp_fee[$matches[1]] = $fee;
-						}
-					}
-
-					// 소속된 그룹을 검사하여 그룹에 맞는 수수료를 구함
-					if(!$is_admin) {
-						if(count($tmp_group) && $logged_info->group_list) {
-							foreach($logged_info->group_list as $key => $val) {
-								if(array_key_exists($key, $tmp_group)) {
-									$config->fee = $tmp_fee[$key];
-									break;
-								}
-							}
-						}
-
-						// 최고 관리자이면 동일 IP 선물 제한 비활성화
-						if($logged_info->is_admin == 'Y') $config->sameip_deny = 'N';
-
-						// 최고 관리자이거나 수수료 제외 그룹에 해당하면 수수료 기능 비활성화
-							if($logged_info->is_admin == 'Y') $config->use_fee = 'N';
-							if(count($config->fee_but_group)) {
-								foreach($logged_info->group_list as $key => $val) {
-									if(in_array($key, $config->fee_but_group)) {
-										$config->use_fee = 'N';
-										break;
-									}
-								}
-							}
-						}
-					}
-				}
-			return $config;
-		}
-
-		/**
-		 * @brief 포인트 선물 권한이 있는지 확인
-		 */
-		function isGranted() {
-			// 로그인 하지 않은 경우 권한이 없음
 			$logged_info = Context::get('logged_info');
-			if(!$logged_info) return false;
 
-			// 설정된 권한이 없을 경우 무조건 권한이 있음
-			$granted = true;
+			$tmp_group = array();
 
-			// 최고 관리자가 아닌 경우 권한이 있는지 확인
-			if($logged_info->is_admin != 'Y') {
-				$granted = true;
-				// 설정을 구함
-				$grant_info = $this->getGrantInfo();
+			if($logged_info)
+			{
+				foreach($config as $key => $val)
+				{
+					if(!preg_match('/^fee_([0-9]+)$/i', $key, $matches)) continue;
 
-				// 설정된 권한이 있다면
-				if(count($grant_info)) {
-					$granted = false;
-					// 그룹이 하나라도 설정된 권한에 포함되어 있다면 권한이 있음
-					foreach($logged_info->group_list as $key => $val) {
-						if(in_array($key, $grant_info)) {
-							$granted = true;
-							break;
-						}
+					$fee = $val < 1 ? 0 : $val;
+					$fee = $val > 99 ? 99 : $val;
+					$config->fee_[$matches[1]] = $fee;
+
+					if(!$is_admin)
+					{
+						$tmp_group[$matches[1]] = $matches[1];
+						$tmp_fee[$matches[1]] = $fee;
 					}
 				}
-			}
 
-			return $granted;
-		}
+				// 소속된 그룹을 검사하여 그룹에 맞는 수수료를 구함
+				if(!$is_admin)
+				{
+					if(count($tmp_group) && $logged_info->group_list)
+					{
+						foreach($logged_info->group_list as $key => $val)
+						{
+							if(array_key_exists($key, $tmp_group))
+							{
+								$config->fee = $tmp_fee[$key];
+								break;
+							}
+						}
+					}
 
-		/**
-		 * @brief log_srl로 포인트 선물 내역 정보 구함
-		 */
-		function getLogInfoByLogSrl($log_srl) {
-			$args->log_srl = $log_srl;
-			$output = executeQuery('pointsend.getPointsendLogInfo',$args);
-			return $output->data;
-		}
+					// 최고 관리자이면 동일 IP 선물 제한 비활성화
+					if($logged_info->is_admin == 'Y') $config->sameip_deny = 'N';
 
-		/**
-		 * @brief 포인트 선물 내역 목록을 구함
-		 **/
-		function getLogList($obj = null) {
-			$args->sender_srl = $obj->sender_srl;
-			$args->receiver_srl = $obj->receiver_srl;
-			$args->sort_index = $obj->sort_index?$obj->sort_index:'regdate';
-			$args->order_type = $obj->order_type?$obj->order_type:'desc';
-			$args->list_count = $obj->list_count?$obj->list_count:20;
-			$args->page_count = $obj->page_count?$obj->page_count:10;
-			$args->page = $obj->page?$obj->page:1;
-			return executeQuery('pointsend.getPointsendLogList',$args);
-		}
+					// 최고 관리자이거나 수수료 제외 그룹에 해당하면 수수료 기능 비활성화
+						if($logged_info->is_admin == 'Y') $config->use_fee = 'N';
+						if(count($config->fee_but_group))
+						{
+							foreach($logged_info->group_list as $key => $val)
+							{
+								if(in_array($key, $config->fee_but_group))
+								{
+									$config->use_fee = 'N';
+									break;
+								}
+							}
+						}
+					}
+				}
+			}
+		return $config;
+	}
+
+	/**
+	 * @brief 포인트 선물 권한이 있는지 확인
+	 */
+	function isGranted()
+	{
+		// 로그인 하지 않은 경우 권한이 없음
+		$logged_info = Context::get('logged_info');
+		if(!$logged_info) return false;
+
+		// 설정된 권한이 없을 경우 무조건 권한이 있음
+		$granted = true;
 
-		/**
-		 * @brief 일괄 포인트 선물 내역 목록을 구함
-		 **/
-		function getBatchLogList($obj = null) {
-			$args->sender_srl = $obj->sender_srl;
-			$args->sort_index = $obj->sort_index?$obj->sort_index:'regdate';
-			$args->order_type = $obj->order_type?$obj->order_type:'desc';
-			$args->list_count = $obj->list_count?$obj->list_count:20;
-			$args->page_count = $obj->page_count?$obj->page_count:10;
-			$args->page = $obj->page?$obj->page:1;
-			return executeQuery('pointsend.getBatchLogList',$args);
-		}
+		// 최고 관리자가 아닌 경우 권한이 있는지 확인
+		if($logged_info->is_admin != 'Y')
+		{
+			$granted = true;
+			// 설정을 구함
+			$grant_info = $this->getGrantInfo();
 
-		/**
-		 * @brief 오늘 보낸/받은 포인트 총 합계를 구함
-		 */
-		function getTodayLog($obj) {
-			if(!$obj || !$obj->member_srl) return;
-			if(!in_array($obj->type,array('S','R'))) $obj->type = 'S';
+			// 설정된 권한이 있다면
+			if(count($grant_info))
+			{
+				$granted = false;
+				// 그룹이 하나라도 설정된 권한에 포함되어 있다면 권한이 있음
+				foreach($logged_info->group_list as $key => $val)
+				{
+					if(in_array($key, $grant_info))
+					{
+						$granted = true;
+						break;
+					}
+				}
+			}
+		}
 
-			$member_srl = $obj->member_srl;
-			$type = $obj->type;
-
-			switch($type) {
-				case 'S':
-					$args->sender_srl = $member_srl;
-					break;
-				case 'R':
-					$args->receiver_srl = $member_srl;
-					break;
-			}
-
-			$args->start_date = date('YmdHis',mktime(0,0,0));
-			$args->end_date = date('YmdHis',mktime(24,59,59));
-
-			$output = executeQuery('pointsend.getTodayPointsend',$args);
-
-			return $output->data;
-		}
-
-		/**
-		 * @brief 금일의 포인트 선물 내역 목록을 구함
-		 * @remarks getTodayLog() 함수와 동작이 같으나 페이징 기능 유무 차이만 있음
-		 */
-		function getTodayLogList($obj) {
-			if(!$obj || $obj->member_srl) return;
-			if(!in_array($obj->type,array('S','R'))) $obj->type = 'S';
-
-			$member_srl = $obj->member_srl;
-			$type = $obj->type;
-
-			switch($type) {
-				case 'S':
-					$args->sender_srl = $member_srl;
-					break;
-				case 'R':
-					$args->receiver_srl = $member_srl;
-					break;
-			}
-
-			$args->start_date = date('YmdHis',mktime(0,0,0));
-			$args->end_date = date('YmdHis',mktime(24,59,59));
-
-			$output = executeQuery('pointsend.getTodayPointsendLogList',$args);
-
-			return $output->data;
-		}
-
-		/**
-		 * @brief 특정 IP의 포인트 선물 내역을 구함
-		 */
-		function getLogsByIpaddress($ipaddress) {
-			if(!$ipaddress) return;
-
-			$args->ipaddress = $ipaddress;
-			$args->order_type = 'desc';
-			return executeQuery('pointsend.getPointsendLogs', $args);
-		}
+		return $granted;
 	}
-?>
+
+	/**
+	 * @brief log_srl로 포인트 선물 내역 정보 구함
+	 */
+	function getLogInfoByLogSrl($log_srl)
+	{
+		$args->log_srl = $log_srl;
+		$output = executeQuery('pointsend.getPointsendLogInfo',$args);
+		return $output->data;
+	}
+
+	/**
+	 * @brief 포인트 선물 내역 목록을 구함
+	 **/
+	function getLogList($obj = null) {
+		$args->sender_srl = $obj->sender_srl;
+		$args->receiver_srl = $obj->receiver_srl;
+		$args->sort_index = $obj->sort_index?$obj->sort_index:'regdate';
+		$args->order_type = $obj->order_type?$obj->order_type:'desc';
+		$args->list_count = $obj->list_count?$obj->list_count:20;
+		$args->page_count = $obj->page_count?$obj->page_count:10;
+		$args->page = $obj->page?$obj->page:1;
+		return executeQuery('pointsend.getPointsendLogList',$args);
+	}
+
+	/**
+	 * @brief 일괄 포인트 선물 내역 목록을 구함
+	 **/
+	function getBatchLogList($obj = null) {
+		$args->sender_srl = $obj->sender_srl;
+		$args->sort_index = $obj->sort_index?$obj->sort_index:'regdate';
+		$args->order_type = $obj->order_type?$obj->order_type:'desc';
+		$args->list_count = $obj->list_count?$obj->list_count:20;
+		$args->page_count = $obj->page_count?$obj->page_count:10;
+		$args->page = $obj->page?$obj->page:1;
+		return executeQuery('pointsend.getBatchLogList',$args);
+	}
+
+	/**
+	 * @brief 오늘 보낸/받은 포인트 총 합계를 구함
+	 */
+	function getTodayLog($obj) {
+		if(!$obj || !$obj->member_srl) return;
+		if(!in_array($obj->type,array('S','R'))) $obj->type = 'S';
+
+		$member_srl = $obj->member_srl;
+		$type = $obj->type;
+
+		switch($type) {
+			case 'S':
+				$args->sender_srl = $member_srl;
+				break;
+			case 'R':
+				$args->receiver_srl = $member_srl;
+				break;
+		}
+
+		$args->start_date = date('YmdHis',mktime(0,0,0));
+		$args->end_date = date('YmdHis',mktime(24,59,59));
+
+		$output = executeQuery('pointsend.getTodayPointsend',$args);
+
+		return $output->data;
+	}
+
+	/**
+	 * @brief 금일의 포인트 선물 내역 목록을 구함
+	 * @remarks getTodayLog() 함수와 동작이 같으나 페이징 기능 유무 차이만 있음
+	 */
+	function getTodayLogList($obj) {
+		if(!$obj || $obj->member_srl) return;
+		if(!in_array($obj->type,array('S','R'))) $obj->type = 'S';
+
+		$member_srl = $obj->member_srl;
+		$type = $obj->type;
+
+		switch($type) {
+			case 'S':
+				$args->sender_srl = $member_srl;
+				break;
+			case 'R':
+				$args->receiver_srl = $member_srl;
+				break;
+		}
+
+		$args->start_date = date('YmdHis',mktime(0,0,0));
+		$args->end_date = date('YmdHis',mktime(24,59,59));
+
+		$output = executeQuery('pointsend.getTodayPointsendLogList',$args);
+
+		return $output->data;
+	}
+
+	/**
+	 * @brief 특정 IP의 포인트 선물 내역을 구함
+	 */
+	function getLogsByIpaddress($ipaddress) {
+		if(!$ipaddress) return;
+
+		$args->ipaddress = $ipaddress;
+		$args->order_type = 'desc';
+		return executeQuery('pointsend.getPointsendLogs', $args);
+	}
+}
+/* End of file : pointsend.model.php */
+/* Location : ./modules/pointsend/poinsend.model.php */
